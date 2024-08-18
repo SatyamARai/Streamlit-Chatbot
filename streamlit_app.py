58e92a5df67f4290fbb55355acad34491a07dc1a
Satyam Ashutosh Rai <100743443+SatyamARai@users.noreply.github.com>
2024-08-18 23:00:11 +0000

    First streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Hello, World! My name is Satyam Ashutosh Rai. This is my first Streamlit App.")
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
